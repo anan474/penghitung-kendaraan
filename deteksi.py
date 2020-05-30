aa2ded8ac79ed279b6586ba6a0b4519975d1cef3
Agung Tuah Ananda <anan474@myself.com>
2020-05-30 22:26:13 +0700

    hubungkan deteksi dengan penghitung

--- deteksi.py
+++ deteksi.py
@@ -3,20 +3,44 @@ import time
 import sys
 import datetime
 
-import cv2
+import cv2 as cv
 import numpy as np
+
+from penghitung import Penghitung
+
+
+# ============================================================================
+
+GARIS_PEMBATAS_KIRI_MASUK = 240
+GARIS_PEMBATAS_KIRI_KELUAR = 60
+
+GARIS_PEMBATAS_KANAN_MASUK = 40
+GARIS_PEMBATAS_KANAN_KELUAR = 120
+
+
+RESIZE_LEBAR = 640
+RESIZE_TINGGI = 360
 
 # ============================================================================
 
 GAMBAR_BACKGROUND = "bg.png"
-VIDEO = "vid.mp4" 
+VIDEO = "vid.mp4"
 
-# Warna untuk digambar di gambar nanti    
-WARNA_GARIS_PEMBELAH = (255, 255, 0)
+# ============================================================================
+
+
+# Warna untuk digambar di gambar nanti
+WARNA_GARIS_PEMBATAS_MASUK = (255, 255, 0)
+WARNA_GARIS_PEMBATAS_KELUAR = (0, 255, 255)
+WARNA_GARIS_PEMBATAS_TENGAH = (0, 0, 0)
+
 WARNA_BOUNDING_BOX = (255, 0, 0)
+WARNA_BOUNDING_BOX_HIJAU = (255, 0, 0)
+
 WARNA_CENTROID = (0, 0, 255)
 
 # ============================================================================
+
 
 def hitung_centroid(x, y, w, h):
     # centroid adalah titik tengah dari objek terdeteksi
@@ -33,50 +57,64 @@ def hitung_centroid(x, y, w, h):
 
 def main():
 
-    gambar_background = cv2.imread(GAMBAR_BACKGROUND)
-    
-    background_subtractor = cv2.createBackgroundSubtractorMOG2(100, 0, True)
+    gambar_background = cv.imread(GAMBAR_BACKGROUND)
+
+    background_subtractor = cv.createBackgroundSubtractorMOG2(100, 0, True)
     background_subtractor.setShadowValue(0)
     background_subtractor.apply(gambar_background, 1)
 
-    video = cv2.VideoCapture(VIDEO)
-    lebar_frame = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-    tinggi_gambar = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    video = cv.VideoCapture(VIDEO)
+    lebar_frame = RESIZE_LEBAR
+    tinggi_frame = RESIZE_TINGGI
+
+    ###
+    penghitung_lajur_kiri = Penghitung(
+        GARIS_PEMBATAS_KIRI_MASUK, GARIS_PEMBATAS_KIRI_KELUAR, "kiri")
+    penghitung_lajur_kanan = Penghitung(
+        GARIS_PEMBATAS_KANAN_MASUK, GARIS_PEMBATAS_KANAN_KELUAR, "kanan")
 
     frame_counter = -1
 
     while True:
+
         frame_counter += 1
 
         ret, frame = video.read()
         if not ret:
             print("Tidak dapat membuka video. Stop.")
             break
-        
+
+        ####
+        # resize frame ke 640 x 360
+        frame = cv.resize(frame, (RESIZE_LEBAR, RESIZE_TINGGI))
+
         ####
         frame_foreground = background_subtractor.apply(frame, 1)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9, 9))
 
-        frame_foreground = cv2.bilateralFilter(frame_foreground,9,75,75)
-        frame_foreground = cv2.morphologyEx(frame_foreground, cv2.MORPH_CLOSE, kernel)
-        frame_foreground = cv2.morphologyEx(frame_foreground, cv2.MORPH_OPEN, kernel)
-        frame_foreground = cv2.dilate(frame_foreground, kernel, iterations = 5)
-        frame_foreground = cv2.erode(frame_foreground, kernel, iterations = 3)
-        frame_foreground = cv2.morphologyEx(frame_foreground, cv2.MORPH_CLOSE, kernel)
+        frame_foreground = cv.bilateralFilter(frame_foreground, 9, 75, 75)
+        frame_foreground = cv.morphologyEx(
+            frame_foreground, cv.MORPH_CLOSE, kernel)
+        frame_foreground = cv.morphologyEx(
+            frame_foreground, cv.MORPH_OPEN, kernel)
+        frame_foreground = cv.dilate(frame_foreground, kernel, iterations=5)
+        frame_foreground = cv.erode(frame_foreground, kernel, iterations=3)
+        frame_foreground = cv.morphologyEx(
+            frame_foreground, cv.MORPH_CLOSE, kernel)
 
         ####
         LEBAR_MINIMAL = 21
         TINGGI_MINIMAL = 21
 
-        _ , contours, _ = cv2.findContours(frame_foreground
-            , cv2.RETR_EXTERNAL
-            , cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, _ = cv.findContours(
+            frame_foreground, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-        objek_ditemukan = []
+        objek_ditemukan_lajur_kiri = []
+        objek_ditemukan_lajur_kanan = []
         for (i, contour) in enumerate(contours):
 
-            (x, y, w, h) = cv2.boundingRect(contour)
+            (x, y, w, h) = cv.boundingRect(contour)
             objek_valid = (w >= LEBAR_MINIMAL) and (h >= TINGGI_MINIMAL)
 
             if not objek_valid:
@@ -84,32 +122,81 @@ def main():
 
             centroid = hitung_centroid(x, y, w, h)
 
-            objek_ditemukan.append(((x, y, w, h), centroid,contour))
+            if centroid[0] < (lebar_frame/2):
+                objek_ditemukan_lajur_kiri.append(((x, y, w, h), centroid))
+            else:
+                objek_ditemukan_lajur_kanan.append(((x, y, w, h), centroid))
 
         ####
-        for (i, objek) in enumerate(objek_ditemukan):
-            posisi, centroid, _ = objek
+        # hubungkan dengan modul penghitung
+        penghitung_lajur_kiri.perbarui_penghitung(objek_ditemukan_lajur_kiri)
+        penghitung_lajur_kanan.perbarui_penghitung(objek_ditemukan_lajur_kanan)
+
+        ####
+        # print jumlah kendaraan terdeteksi
+        os.system("clear")
+        print("frame ke: ", frame_counter)
+        print("lajur kiri: ", penghitung_lajur_kiri.jumlah_kendaraan)
+        print("lajur kanan:", penghitung_lajur_kanan.jumlah_kendaraan)
+
+        ####
+        # gambar objek terdeteksi dan centroid nya
+        for (i, objek) in enumerate(objek_ditemukan_lajur_kanan + objek_ditemukan_lajur_kiri):
+            posisi, centroid = objek
             x, y, w, h = posisi
 
-            # gambar bounding box pada gambar 
-            cv2.rectangle(frame, (x, y), (x + w - 1, y + h - 1), WARNA_BOUNDING_BOX, 1)
-        
-            # gambar centroid pada gambar 
-            cv2.circle(frame, centroid, 2, WARNA_CENTROID, -1)
-            
+            # gambar bounding box pada frame
+            cv.rectangle(frame, (x, y), (x + w - 1, y + h - 1),
+                         WARNA_BOUNDING_BOX, 1)
+
+            # gambar centroid pada frame
+            cv.circle(frame, centroid, 2, WARNA_CENTROID, -1)
 
         ####
-        cv2.imshow('asli', frame)
-        cv2.imshow('foreground', frame_foreground)
+        # gambar objek terdeteksi dan centroid nya, dari penghitung kendaraan
+        for (i, objek) in enumerate(objek_ditemukan_lajur_kanan + objek_ditemukan_lajur_kiri):
+            posisi, centroid = objek
+            x, y, w, h = posisi
 
-        c = cv2.waitKey(250)
+            # gambar bounding box pada frame
+            cv.rectangle(frame, (x, y), (x + w - 1, y + h - 1),
+                         WARNA_BOUNDING_BOX, 1)
+
+            # gambar centroid pada frame
+            cv.circle(frame, centroid, 2, WARNA_CENTROID, -1)
+
+        ####
+        # gambar garis pembatas kiri, kanan, dan tengah
+
+        # kiri
+        cv.line(frame, (0, GARIS_PEMBATAS_KIRI_MASUK), (int(lebar_frame/2), GARIS_PEMBATAS_KIRI_MASUK),
+                WARNA_GARIS_PEMBATAS_MASUK, 1)
+        cv.line(frame, (0, GARIS_PEMBATAS_KIRI_KELUAR), (int(lebar_frame/2), GARIS_PEMBATAS_KIRI_KELUAR),
+                WARNA_GARIS_PEMBATAS_KELUAR, 1)
+
+        # kanan
+        cv.line(frame, (int(lebar_frame/2), GARIS_PEMBATAS_KANAN_MASUK), (int(lebar_frame), GARIS_PEMBATAS_KANAN_MASUK),
+                WARNA_GARIS_PEMBATAS_MASUK, 1)
+        cv.line(frame, (int(lebar_frame/2), GARIS_PEMBATAS_KANAN_KELUAR), (int(lebar_frame), GARIS_PEMBATAS_KANAN_KELUAR),
+                WARNA_GARIS_PEMBATAS_KELUAR, 1)
+
+        # tengah
+        cv.line(frame, (int(lebar_frame/2), 0), (int(lebar_frame/2), tinggi_frame),
+                WARNA_GARIS_PEMBATAS_TENGAH, 1)
+
+        ####
+        cv.imshow('asli', frame)
+        # cv.imshow('foreground', frame_foreground)
+
+        ###
+        c = cv.waitKey(0)
         if c == 27:
             print("ESC ditekan, menghentikan program.")
             break
 
     print("Menutup video ...")
     video.release()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
     print("Selesai .")
 
 
--- penghitung.py
+++ penghitung.py
@@ -1,39 +1,44 @@
 import math
 
-import cv2
 import numpy as np
 
 # ============================================================================
 
-WARNA_GARIS = [ (0,0,255), (0,106,255), (0,216,255), (0,255,182), (0,255,76)
-    , (144,255,0), (255,255,0), (255,148,0), (255,0,178), (220,0,255) ]
+WARNA_GARIS = [(0, 0, 255), (0, 106, 255), (0, 216, 255), (0, 255, 182), (0, 255, 76),
+               (144, 255, 0), (255, 255, 0), (255, 148, 0), (255, 0, 178), (220, 0, 255)]
 
 WARNA_BOUNDING_BOX = (255, 0, 0)
 
+
 # ============================================================================
+
 
 class Kendaraan(object):
-    def __init__(self, id, centroid)
+    def __init__(self, id, centroid, posisi):
         self.id = id
 
-        self.posisi = [centroid]
+        self.centroid = [centroid]
         self.tidak_terlihat = 0
         self.telah_dihitung = False
-        
+        self.posisi = posisi
+
     @property
-    def posisi_terakhir(self):
-        return self.posisi[-1]
+    def centroid_terakhir(self):
+        return self.centroid[-1]
+
+    def perbarui_centroid(self, centroid_terbaru):
+        self.centroid.append(centroid_terbaru)
+        self.tidak_terlihat = 0
 
     def perbarui_posisi(self, posisi_terbaru):
-        self.posisi.append(posisi_terbaru)
+        self.posisi = posisi_terbaru
         self.tidak_terlihat = 0
 
 # ============================================================================
 
+
 class Penghitung (object):
-    def __init__(self, tinggi_frame, lebar_frame , garis_masuk, garis_keluar, lajur):
-        self.tinggi_frame = tinggi_frame
-        self.lebar_frame = lebar_frame
+    def __init__(self, garis_masuk, garis_keluar, lajur):
         self.garis_masuk = garis_masuk
         self.garis_keluar = garis_keluar
 
@@ -42,10 +47,11 @@ class Penghitung (object):
         self.kendaraan = []
         self.id_kendaraan_selanjutnya = 0
         self.jumlah_kendaraan = 0
-        self.limit_tidak_terlihat = 10 # jika pada @limit frame tidak terlihat maka kendaraan dihilangkan
+        # jika pada @limit frame tidak terlihat maka kendaraan dihilangkan
+        self.limit_tidak_terlihat = 10
 
     @staticmethod
-    def hitung_vektor(a,b):
+    def hitung_vektor(a, b):
         # Hitung vektor antara titik satu ke titik lainnya. Sudut 0 merupakan tegak lurus dari atas ke bawah, sudut bergerak searah jarum jam
         dx = float(b[0] - a[0])
         dy = float(b[1] - a[1])
@@ -67,63 +73,69 @@ class Penghitung (object):
             elif dx > 0:
                 sudut = -180 - math.degrees(math.atan(dx/dy))
             else:
-                sudut = 180.0        
+                sudut = 180.0
 
-        return jarak, sudut 
+        return jarak, sudut
+
+    @staticmethod
+    def apakah_vektor_valid(a):
+        jarak, sudut = a
+        batas_jarak = max(40.0, -0.008 * sudut**2 + 0.4 * sudut + 25.0)
+        return (jarak <= batas_jarak)
 
     def apakah_masuk_garis(self, centroid):
         # jika centroid berada dalam garis sesuai dengan lajur nya maka kembalikan nilai True
         if self.lajur is "kiri" and (centroid[1] < self.garis_masuk) and (centroid[1] > self.garis_keluar):
             return True
-        elif self.lajur is "kanan" and (centroid[1] > self.garis_masuk) and (centroid[1] > self.garis_keluar):
+        elif self.lajur is "kanan" and (centroid[1] > self.garis_masuk) and (centroid[1] < self.garis_keluar):
             return True
         else:
             return False
 
     def apakah_melewati_garis_keluar(self, kendaraan):
-        # jika kendararan telah melewati garis keluar sesuai jalurnya maka kembalikan nilai True
-        if self.jalur is "kiri" and (kendaraan.posisi_terakhir[1] < self.garis_keluar):
+        # jika kendararan telah melewati garis keluar sesuai lajur nya maka kembalikan nilai True
+        if self.lajur is "kiri" and (kendaraan.centroid_terakhir[1] < self.garis_keluar):
             return True
-        elif self.jalur is "kanan" and (kendaraan.posisi_terakhir[1] > self.garis_keluar):
+        elif self.lajur is "kanan" and (kendaraan.centroid_terakhir[1] > self.garis_keluar):
             return True
         else:
             return False
 
+    def perbarui_penghitung(self, objek):
 
-    def perbarui_penghitung(self, objek, potongan_gambar):
-        
-        # dari data kendaraan yang telah di-tracking, cek satu persatu mana yang merupakan kendaraan tersebut        
+        # dari data kendaraan yang telah di-tracking, cek satu persatu mana yang merupakan kendaraan tersebut
         for indek_kendaraan, kendaraan in enumerate(self.kendaraan):
 
-            ### update status kendaraan
+            # update status kendaraan
             #  jika cocok update status kendaraan nya,
             #  jika tidak ada cocok dengan kendaraan yang sudah ada maka tambahkan jadi kendaraan baru
             ada_objek_yg_cocok = False
             for indek_objek_ini, objek_ini in enumerate(objek):
                 posisi_objek_ini, centroid_objek_ini = objek_ini
 
-                vektor = self.hitung_vektor(kendaraan.posisi_terakhir, centroid_objek_ini)
+                vektor = self.hitung_vektor(
+                    kendaraan.centroid_terakhir, centroid_objek_ini)
 
                 if self.apakah_vektor_valid(vektor):
                     # update status kendaraan
-                    kendaraan.perbarui_posisi(centroid_objek_ini)
+                    kendaraan.perbarui_centroid(centroid_objek_ini)
                     # hilangkan dari daftar objek
                     del objek[indek_objek_ini]
                     # set bahwa ada objek yang cocok untuk kendaraan ini
                     ada_objek_yg_cocok = True
 
-                    ## sampai disini sebenarnya bisa di hentikan perulangan nya tapi coba biarkan untuk cek apakah bisa terdeteksi menjadi lebih dari dua kendaraan untuk objek yang sama
-            
+                    # sampai disini sebenarnya bisa di hentikan perulangan nya tapi coba biarkan untuk cek apakah bisa terdeteksi menjadi lebih dari dua kendaraan untuk objek yang sama
+
             # kalau tidak ada ketemu tambahkan hitungan jumlah frame tidak terlihat untuk kendaraan ini
             if ada_objek_yg_cocok is False:
                 kendaraan.tidak_terlihat += 1
 
-            ### hitung kendaraan yang belum dihitung dan melewati garis batas keluar 
-            if not kendaraan.telah_dihitung and apakah_melewati_garis_keluar(kendaraan):
+            # hitung kendaraan yang belum dihitung dan melewati garis batas keluar
+            if not kendaraan.telah_dihitung and self.apakah_melewati_garis_keluar(kendaraan):
                 self.jumlah_kendaraan += 1
                 kendaraan.telah_dihitung = True
 
-            ### hapus kendaraan yang telah melewati limit batas nilai tidak terlihat di frame
+            # hapus kendaraan yang telah melewati limit batas nilai tidak terlihat di frame
             if kendaraan.tidak_terlihat > self.limit_tidak_terlihat:
                 del self.kendaraan[indek_kendaraan]
 
@@ -132,10 +144,8 @@ class Penghitung (object):
             posisi_objek_ini, centroid_objek_ini = objek_ini
 
             # cek apakah objek berada dalam area yang dibatasi garis
-            if apakah_masuk_garis(centroid_objek_ini):
-                kendaraan_baru = Kendaraan(self.id_kendaraan_selanjutnya, centroid_objek_ini, posisi_objek_ini)
+            if self.apakah_masuk_garis(centroid_objek_ini):
+                kendaraan_baru = Kendaraan(
+                    self.id_kendaraan_selanjutnya, centroid_objek_ini, posisi_objek_ini)
                 self.id_kendaraan_selanjutnya += 1
                 self.kendaraan.append(kendaraan_baru)
-
-
-        
