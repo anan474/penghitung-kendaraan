863dcf1a38ac381b2ee0e3001eac8d48d56e745c
Agung Tuah Ananda <anan474@myself.com>
2021-10-23 21:19:28 +0700

    gak tau deh apa yang berubah

--- penghitungkendaraan/pendeteksi_objek.py
+++ penghitungkendaraan/pendeteksi_objek.py
@@ -16,7 +16,7 @@ class PendeteksiObjek():
     """Kelas ini melakukan proses terhadap citra asli untuk mengekstrak daftar objek kendaraan yang ada dengan melakukan Background Subtraction dan proses morfologi.
     """
 
-    def __init__(self,config):
+    def __init__(self, config):
 
         self.config = config
 
@@ -27,44 +27,41 @@ class PendeteksiObjek():
         self.background_subtractor.apply(gambar_background, 1)
 
         self.lebar_frame = RESIZE_LEBAR
-        self.tinggi_frame = RESIZE_TINGGI
 
-        self.kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-        self.kernel2 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
+        self.kernel3 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+        self.kernel7 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
         self.kernel9 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9, 9))
 
         logger.debug("init module")
 
-    def get_foreground(self, frame):
+    def __get_foreground(self, frame):
         frame_foreground = self.background_subtractor.apply(frame, 1)
-        
+
         return frame_foreground
 
-    def proses_morfologi(self, frame):
+    def __proses_morfologi(self, frame):
 
         frame = cv.bilateralFilter(
             frame, 9, 75, 75)
 
-
         # frame_foreground = cv.morphologyEx(
         #     frame, cv.MORPH_OPEN, self.kernel9)
 
-
-        # frame = cv.erode(frame, self.kernel, iterations=3)
-        frame = cv.erode(frame, self.kernel, iterations=1)
-        frame = cv.dilate(frame, self.kernel, iterations=1)
+        # frame = cv.erode(frame, self.kernel3, iterations=3)
+        frame = cv.erode(frame, self.kernel3, iterations=1)
+        frame = cv.dilate(frame, self.kernel3, iterations=1)
         frame = cv.morphologyEx(
-            frame, cv.MORPH_CLOSE, self.kernel2)
+            frame, cv.MORPH_CLOSE, self.kernel7)
 
         # frame = cv.morphologyEx(
-        #     frame, cv.MORPH_CLOSE, self.kernel)
+        #     frame, cv.MORPH_CLOSE, self.kernel3)
 
         if (self.config['cetakgambar']['morfologi']):
             cv.imshow('morfologi', frame)
 
         return frame
 
-    def hitung_centroid(self, x, y, w, h):
+    def __hitung_centroid(self, x, y, w, h):
         # centroid adalah titik tengah dari objek terdeteksi
         x1 = int(w / 2)
         y1 = int(h / 2)
@@ -74,7 +71,7 @@ class PendeteksiObjek():
 
         return (cx, cy)
 
-    def dapatkan_objek(self, frame):
+    def __dapatkan_objek(self, frame):
         contours, _ = cv.findContours(
             frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
@@ -87,7 +84,7 @@ class PendeteksiObjek():
             if not objek_valid:
                 continue
 
-            centroid = self.hitung_centroid(x, y, w, h)
+            centroid = self.__hitung_centroid(x, y, w, h)
 
             lajur = "kiri" if centroid[0] < (self.lebar_frame/2) else "kanan"
 
@@ -98,9 +95,8 @@ class PendeteksiObjek():
 
     def deteksi_objek(self, frame):
 
-        foreground = self.get_foreground(frame)
-        foreground = self.proses_morfologi(foreground)
-
-        daftar_objek = self.dapatkan_objek(foreground)
+        foreground = self.__get_foreground(frame)
+        foreground = self.__proses_morfologi(foreground)
+        daftar_objek = self.__dapatkan_objek(foreground)
 
         return daftar_objek, foreground
